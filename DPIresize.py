e887ab822e06b0cfe5f295f61e0c01ef39758d62
Melissa <melissa.r.dale>
2019-12-11 20:55:36 -0500

    simple script to change image dpi and example files

---
+++ DPIresize.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os
+import glob
+
+if not os.path.exists('./changed_dpi/'):
+    os.makedirs('./changed_dpi/')
+
+change_dpi = glob.glob('./*.jpg')
+
+for im in change_dpi:
+    new_im = Image.open(im)
+    print(new_im.info['dpi'])
+
+    new_im.save('./changed_dpi/' + im, dpi=(500,500))
+
